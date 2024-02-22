cce72cac1d3dc38c42209da5123d7baed6d84278
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-02-22 10:39:53 +0200

    Add project files.

---
+++ L1Ex1won7/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+paralelipiped dreptunghic, va afisa volumul lui*/
+
+int alt;
+Console.WriteLine("Introduce inaltimea: ");
+alt = Convert.ToInt32(Console.ReadLine());
+
+int lag;
+Console.WriteLine("Introduce latimea: ");
+lag = Convert.ToInt32(Console.ReadLine());
+
+int comp;
+Console.WriteLine("Introduce lungimea: ");
+comp = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("OVolumul paralelepipedului este: " + alt * lag * comp);
+
+Console.ReadLine();
